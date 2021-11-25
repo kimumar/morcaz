200adf4f2d1d30c5f84ef216d75aa4bcf7b65b8c
umar <kimumar55@gmail.com>
2021-11-25 10:35:48 +0100

    gr

---
+++ alpha/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
